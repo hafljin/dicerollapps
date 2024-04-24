c1153cae1b9b0e803712cb8dc96d4d77af4eeec3
Mic <michelnativedeveloper@gmail.com>
2024-04-24 19:10:23 +0900

    ボタン押したら乱数がテキストに反映されるを作成

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    //Buttonが押されたらランダム値を生成してテキストに反映させる関数
+    private fun rollDice() {
+        val resultText = findViewById<TextView>(R.id.result_text)
+        //1~7までの乱数を生成する
+        val randomInt = Random.nextInt(1, 7)
+        //生成した乱数をテキストにキャストする
+        resultText.text = randomInt.toString()
     }
 }
